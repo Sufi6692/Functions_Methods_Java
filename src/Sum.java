388369b40b430fe3a1b2e3cdb8adb4fe80bb6139
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-10 20:50:06 +0530

    Started Understanding Function using Sum operation

--- src/Sum.java
+++ src/Sum.java
@@ -1,18 +1,17 @@
-import java.awt.*;
 import java.util.Scanner;
 
-public  class Main  {
+public class Sum {
     public static void main(String[] args) {
-        // Q: take input of 2 numbers and print the sum
-        Scanner input = new Scanner(System.in);
+        sum();
+    }
 
+   static void sum(){
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter number 1: ");
-       int num1 = input.nextInt();
+        int num1 = input.nextInt();
         System.out.print("Enter number 2: ");
         int num2 = input.nextInt();
         int sum = num1 + num2;
         System.out.println("The sum is :"+ sum);
-
-
     }
 }
