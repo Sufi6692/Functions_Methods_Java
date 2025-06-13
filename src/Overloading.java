e9917f32cd442ca5b6fc025e8b86c0e996cc3c37
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-13 21:50:10 +0530

    Understand about the Overloading with help Function and Methods in Java

---
+++ src/Overloading.java
@@ -0,0 +1,29 @@
+public class Overloading {
+    public static void main(String[] args) {
+        fun("sufi");
+        fun(34);
+
+        int ans = sum(4,5);
+    }
+
+    static int sum(int a , int b){
+
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c){
+        return  a + b + c;
+    }
+
+    static void fun(int a){
+        System.out.println("first one");
+        System.out.println(a);
+
+    }
+
+    static void fun(String name){
+        System.out.println("second one");
+        System.out.println(name);
+
+    }
+}
